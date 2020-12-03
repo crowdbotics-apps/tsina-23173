202eb70f8ea41f9ced25f787112d8b5948bf23d3
Crowdbotics <team@crowdbotics.com>
2020-12-03 14:57:51 +0000

    Update models

--- backend/tsina_23173/wsgi.py
+++ backend/tsina_23173/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tsina_23173.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tsina_23173.settings")
 
 application = get_wsgi_application()
